5932706b65d7590da92567727636cbe39ead05d3
LightPat <60397186+LightPat@users.noreply.github.com>
2022-04-30 14:59:50 -0400

    Calculate distance between thumb and index fingertips

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Sat Apr 30 12:55:27 2022
 import cv2
 import mediapipe as mp
 import time
+import math
 
 cam = cv2.VideoCapture(0)
 # Create window for frames from webcam to be displayed in
@@ -21,8 +22,8 @@ pTime = 0
 # Create hand tracking model
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
-                      min_detection_confidence=0.5,
+                      max_num_hands=1,
+                      min_detection_confidence=0.3,
                       min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
 
@@ -41,14 +42,31 @@ while True:
     
     handPosition = hands.process(frame)
     
+    distThumbIndex = 0
+    startPoint = (0,0)
+    
     if handPosition.multi_hand_landmarks:
         for landmarkList in handPosition.multi_hand_landmarks:
             for ID, lm in enumerate(landmarkList.landmark):
+                # Height, width, color
                 h, w, c = frame.shape
+                # Scale coordinates according to size of frame
                 cx, cy = int(lm.x *w), int(lm.y*h)
-                cv2.circle(frame, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                # ID 8 is the tip of the index finger
+                # ID 4 is the tip of the thumb
                 
+                if ID == 4 or ID == 8:
+                    cv2.circle(frame, (cx,cy), 3, (0,255,0), cv2.FILLED)
+                    if startPoint != (0,0):
+                        # cv2.line(frame, startPoint, (cx, cy), (0,0,0), 3)
+                        distThumbIndex = math.dist(startPoint, (cx, cy))
+                    startPoint = (cx, cy)
+                else:
+                    cv2.circle(frame, (cx,cy), 3, (255,0,0), cv2.FILLED)
             mpDraw.draw_landmarks(frame, landmarkList, mpHands.HAND_CONNECTIONS)
+    
+    if (distThumbIndex != 0):    
+        print(distThumbIndex)
     
     # Display in window
     cv2.imshow("Hand Tracking", frame)
