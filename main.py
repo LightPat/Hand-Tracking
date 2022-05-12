a09693de79cbc64623d1f505536c76563feeb00d
LightPat <60397186+LightPat@users.noreply.github.com>
2022-05-12 00:54:24 -0400

    Cleaning up before pres

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ import screen_brightness_control as sbc
 cam = cv2.VideoCapture(0)
 # Create window for frames from webcam to be displayed in
 cv2.namedWindow("Hand Tracking")
-# Used for naming images if we save frames using the SPACE key
-img_counter = 0
 # Used for calculating FPS
 cTime = 0
 pTime = 0
@@ -44,8 +42,10 @@ while True:
     pTime = cTime
     cv2.putText(frame, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
+    # Run predictions using mediapipe models
     handPosition = hands.process(frame)
     
+    # Initialize variables for later
     distThumbIndex = 0
     startPoint = (0,0)
     
@@ -56,19 +56,23 @@ while True:
                 h, w, c = frame.shape
                 # Scale coordinates according to size of frame
                 cx, cy = int(lm.x *w), int(lm.y*h)
+                
                 # ID 8 is the tip of the index finger
                 # ID 4 is the tip of the thumb
-                
                 if ID == 4 or ID == 8:
+                    # Draw green cirlces for thumb and index
                     cv2.circle(frame, (cx,cy), 3, (0,255,0), cv2.FILLED)
+                    # Need to check if startPoint has been assigned yet
                     if startPoint != (0,0):
-                        # cv2.line(frame, startPoint, (cx, cy), (0,0,0), 3)
+                        # Calculate scaled distance
                         distThumbIndex = math.dist(startPoint, (cx, cy))
                     startPoint = (cx, cy)
                 else:
+                    # Draw blue circles for every other point
                     cv2.circle(frame, (cx,cy), 3, (255,0,0), cv2.FILLED)
-            mpDraw.draw_landmarks(frame, landmarkList, mpHands.HAND_CONNECTIONS)
+            # mpDraw.draw_landmarks(frame, landmarkList, mpHands.HAND_CONNECTIONS)
     
+    # If distance has been assigned, change the brightness of the primary display
     if (distThumbIndex != 0):
         sbc.set_brightness(distThumbIndex/240*100, display=0)
     
@@ -76,16 +80,10 @@ while True:
     cv2.imshow("Hand Tracking", frame)
 
     k = cv2.waitKey(1)
-    if k%256 == 27:
+    if k == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
-        # SPACE pressed, save frame to disk
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
 
 cam.release()
 
